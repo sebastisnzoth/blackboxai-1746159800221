ea0936971d6b766cbb89258a3c0b695853358f03
Sergio Sebastian Taboada <31399951+sebastisnzoth@users.noreply.github.com>
2025-05-02 01:23:58 -0300

    Add ugo-app/app/types.ts 1746159837464

---
+++ ugo-app/app/types.ts
@@ -0,0 +1,8 @@
+export type RootStackParamList = {
+  Login: undefined;
+  Register: undefined;
+  Home: undefined;
+  Chat: undefined;
+  Services: undefined;
+  ServiceDetail: { service: { id: string; name: string } };
+};
